1c6eded86eeb68027bc53cc5042d3f3755954136
Bryan Schneller <bryan.schneller@gmail.com>
2024-01-05 18:47:30 -0500

    Streamline class structure more

--- Assets/script.js
+++ Assets/script.js
@@ -1,48 +1,13 @@
-//gets the time div, text field, and button elements from HTML
-const nineAmDiv = document.querySelector("#hour-9");
-const tenAmDiv = document.querySelector("#hour-10");
-const elevenAmDiv = document.querySelector("#hour-11");
-const twelvePmDiv = document.querySelector("#hour-12");
-const onePmDiv = document.querySelector("#hour-1");
-const twoPmDiv = document.querySelector("#hour-2");
-const threePmDiv = document.querySelector("#hour-3");
-const fourPmDiv = document.querySelector("#hour-4");
-const fivePmDiv = document.querySelector("#hour-5");
-
-const nineText = document.querySelector("#text9");
-const tenText = document.querySelector("#text10");
-const elevenText = document.querySelector("#text11");
-const twelveText = document.querySelector("#text12");
-const oneText = document.querySelector("#text1");
-const twoText = document.querySelector("#text2");
-const threeText = document.querySelector("#text3");
-const fourText = document.querySelector("#text4");
-const fiveText = document.querySelector("#text5");
-
-const nineBut = document.querySelector("#but9");
-const tenBut = document.querySelector("#but10");
-const elevenBut = document.querySelector("#but11");
-const twelveBut = document.querySelector("#but12");
-const oneBut = document.querySelector("#but1");
-const twoBut = document.querySelector("#but2");
-const threeBut = document.querySelector("#but3");
-const fourBut = document.querySelector("#but4");
-const fiveBut = document.querySelector("#but5");
-
-//gets date div element from HTML, sets the current formatted day/date as the text content
 const currentTime = document.querySelector(".lead");
 currentTime.textContent = dayjs().format("dddd MMMM DD, YYYY");
-//current hour in 24-hour time for comparison
 let currentHour = dayjs().format("H");
 
-//creates a Divider class
 const HourDivider = class {
     #index
     #time
     #div
     #text
     #button
-    storageData
     
     constructor(index, time, div, text, button) {
         this.#index = index;
@@ -64,7 +29,6 @@ const HourDivider = class {
     saveText = () => {
         let data = this.getText().value;
         localStorage.setItem(this.#index, data);
-        console.log(data)
     }
     getButton(){
         return this.#button;
@@ -72,26 +36,27 @@ const HourDivider = class {
     setText(text){
         this.#text.textContent = text;
     }
+    auto = () => {
+        this.updateColor();
+        this.setText(localStorage.getItem(this.#index));
+        this.getButton().addEventListener("click", this.saveText);
+    }
 }
 
-//instantiate class objects for each hour
-const nineAm = new HourDivider(0, 9, nineAmDiv, nineText, nineBut);
-const tenAm = new HourDivider(1, 10, tenAmDiv, tenText, tenBut);
-const elevenAm = new HourDivider(2, 11, elevenAmDiv, elevenText, elevenBut);
-const twelvePm = new HourDivider(3, 12, twelvePmDiv, twelveText, twelveBut);
-const onePm = new HourDivider(4, 13, onePmDiv, oneText, oneBut);
-const twoPm = new HourDivider(5, 14, twoPmDiv, twoText, twoBut);
-const threePm = new HourDivider(6, 15, threePmDiv, threeText, threeBut);
-const fourPm = new HourDivider(7, 16, fourPmDiv, fourText, fourBut);
-const fivePm = new HourDivider(8, 17, fivePmDiv, fiveText, fiveBut);
+const nineAm = new HourDivider(0, 9, document.querySelector("#hour-9"), document.querySelector("#text9"), document.querySelector("#but9"));
+const tenAm = new HourDivider(1, 10, document.querySelector("#hour-10"), document.querySelector("#text10"), document.querySelector("#but10"));
+const elevenAm = new HourDivider(2, 11, document.querySelector("#hour-11"), document.querySelector("#text11"), document.querySelector("#but11"));
+const twelvePm = new HourDivider(3, 12, document.querySelector("#hour-12"), document.querySelector("#text12"), document.querySelector("#but12"));
+const onePm = new HourDivider(4, 13, document.querySelector("#hour-1"), document.querySelector("#text1"), document.querySelector("#but1"));
+const twoPm = new HourDivider(5, 14, document.querySelector("#hour-2"), document.querySelector("#text2"), document.querySelector("#but2"));
+const threePm = new HourDivider(6, 15, document.querySelector("#hour-3"), document.querySelector("#text3"), document.querySelector("#but3"));
+const fourPm = new HourDivider(7, 16, document.querySelector("#hour-4"), document.querySelector("#text4"), document.querySelector("#but4"));
+const fivePm = new HourDivider(8, 17, document.querySelector("#hour-5"), document.querySelector("#text5"), document.querySelector("#but5"));
 
 let arrayHours = [nineAm, tenAm, elevenAm, twelvePm, onePm, twoPm,
      threePm, fourPm, fivePm];
 
-for (let i = 0; i < arrayHours.length; i++){
-    arrayHours[i].updateColor();
-    arrayHours[i].storageData = localStorage.getItem(i);
-    arrayHours[i].setText(arrayHours[i].storageData);
-    arrayHours[i].getButton().addEventListener("click", arrayHours[i].saveText);
+for (hour of arrayHours){
+    hour.auto();
 }
 
